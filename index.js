5f254b5560c8f08ed6e11c154f88c509210abcb8
Goodness Adesoye <104650487+Guddyade@users.noreply.github.com>
2022-10-23 15:24:25 +0000

    new changes

--- index.js
+++ index.js
@@ -1,24 +1,45 @@
-alert("Welcome to Number Guessing Game \n Guess between 1 and 2");
-num = 0;
-let username = prompt("Enter your username: ");
-let randomNumber = Math.ceil(Math.random() * 2);
-console.log(randomNumber);
-playerGuess = prompt("Enter your number guess: ");
+alert("Welcome to the Number Guessing Game. Goodluck! \n");
+const username = prompt("Enter your username: ");
+console.log(`Welcome ${username}. Get ready to play`);
+let points = -1;
+let levels = 1;
+let maxStage = 10;
+let moveOn = false;
 
-for (i = 0; i < 10; i++) {
-  if (randomNumber == playerGuess) {
-    alert(`Correct! \n You've earned ${num + 1} point`);
+
+for (i = 2; i < maxStage; i++){
+  // let randomNumber = Math.ceil(Math.random() * 2);
+   let randomNumber = Math.floor(Math.random() * i) + 1;
+console.log(randomNumber);
+let playerGuess = parseInt(prompt(`Enter a number between 1 and ${i}`));
+  if(randomNumber == playerGuess){
+    console.log(`Great \n You've earned ${points + i} points \n`);
+    
+   console.log(`Welcome to level ${levels+i}`);
+  }
+  else{
+    console.log(`Ouch! \n You're wrong. Do you want to start ${i-1} again? `)
+     break;   
+    
+}
+if(moveOn = !true){
+      
+  }}
+
+
+
+  // if (randomNumber == playerGuess) {
+  //   alert(`Correct! \n You've earned ${num + 1} point`);
 
     //  let randomNumber =  Math.ceil(Math.random()*3);
     //  playerGuess = prompt("Enter your number guess: ");
     //  if(randomNumber == playerGuess){
     //   alert(`Correct! \n You've earned ${num+1} point`)
     //  }
-  }
+  // }
   // else if(){
 
   // }
-  else {
-    alert("Oops! You're out. Kindly refresh to start");
-  }
-}
+//   else {
+//     alert("Oops! You're out. Kindly refresh to start");
+//   }
